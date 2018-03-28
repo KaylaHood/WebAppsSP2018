901fff19a4c93e434678da2f780cdbbc1d93bf1e
Kayla Hood <kaylahood1996@gmail.com>
2018-03-28 12:34:06 -0500

    Added redirecting javascript

--- Views/Sprites.php
+++ Views/Sprites.php
@@ -1,4 +1,1 @@
-<div class="fullscreen">
-    <iframe class="fullscreen" src="http://pandora02.cs.trinity.edu:9130/">
-    </iframe>
-</div>
+<script type="text/javascript" src="Scripts/sprites.js"></script>
